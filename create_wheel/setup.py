d68401053fa1e9e62d8e60aad064ae29e0b44a65
banderlog <mailrupile@gmail.com>
2019-03-11 20:12:39 +0200

    wheel setup update

--- create_wheel/setup.py
+++ create_wheel/setup.py
@@ -5,15 +5,24 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
+# This creates a list which is empty but returns a length of 1.
+# Should make the wheel a binary distribution and platlib compliant.
+#<https://github.com/skvark/opencv-python/blob/master/setup.py>
+class EmptyListWithLength(list):
+    def __len__(self):
+        return 1
+
+
 setuptools.setup(
     name="opencv-python-inference-engine",
-    version="4.0.1.2",
+    version="4.0.1.3",
     url="https://github.com/banderlog/opencv-python-inference-engine",
     maintainer="Kabakov Borys",
     license='MIT, BSD',
     description="Wrapper package for OpenCV with Inference Engine python bindings",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    ext_modules=EmptyListWithLength(),
     packages=['cv2'],
     package_data={'cv2': ['*.so']},
     include_package_data=True,
@@ -28,8 +37,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: C++',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Topic :: Software Development',
