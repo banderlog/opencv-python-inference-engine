9a24f21f16f743e2c2892f520c0e1a10e6906822
banderlog <b.a.kabakov@gmail.com>
2020-02-23 23:24:10 +0200

    downloadautomatisation script

---
+++ download_all_stuff.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# colors
+end="\033[0m"
+red="\033[0;31m"
+green="\033[0;32m"
+
+green () {
+  echo -e "${green}${1}${end}"
+}
+
+red () {
+  echo -e "${red}${1}${end}"
+}
+
+green "DOWNLOAD ALL STUFF"
+wget -c https://github.com/opencv/opencv/archive/4.2.0.tar.gz 
+wget -c https://github.com/FFmpeg/FFmpeg/archive/n4.2.2.tar.gz
+wget -c https://github.com/opencv/dldt/archive/2020.1.tar.gz
+
+green "CLEAN LIB DIRS"
+rm -drf ./dldt/*
+rm -drf ./ffmpeg/*
+rm -drf ./opencv/*
+
+green "UNZIP ALL STUFF"
+tar -xf 2020.1.tar.gz --strip-components=1 -C ./dldt/
+tar -xf n4.2.2.tar.gz --strip-components=1 -C ./ffmpeg/
+tar -xf 4.2.0.tar.gz --strip-components=1 -C ./opencv/
+
+green "GIT RESET FOR ade"
+cd ./dldt/inference-engine/thirdparty/ade
+git clone https://github.com/opencv/ade/ ./
+git reset --hard cbe2db6
+
+green "GIT RESET FOR ngraph"
+cd ../../../ngraph
+git clone https://github.com/NervanaSystems/ngraph ./
+git reset --hard b0bb801
+
+green "CREATE VENV"
+cd ../../
+
+if [[ ! -d ./venv ]]; then
+	virtualenv --clear --always-copy -p /usr/bin/python3 ./venv
+	./venv/bin/pip3 install numpy
+fi
