b99060b73f6277e80e1aefb83d559ad1956b81c8
banderlog <b.a.kabakov@gmail.com>
2021-02-28 18:03:09 +0200

    restore changes in shallow submodules

--- download_all_stuff.sh
+++ download_all_stuff.sh
@@ -20,12 +20,14 @@ ROOT_DIR=$(pwd)
 green "RESET GIT SUBMODULES"
 # use `git fetch --unshallow && git checkout tags/<tag>` for update
 git submodule update --init --recursive --depth=1 --jobs=4
+# restore changes
+git submodule foreach --recursive git restore .
 
 green "CLEAN BUILD DIRS"
 find build/dldt/ -mindepth 1 -not -name 'dldt_setup.sh' -not -name '*.patch' -delete
 find build/opencv/ -mindepth 1 -not -name 'opencv_setup.sh' -delete
-find build/ffmpeg/ -mindepth 1 -not -name 'ffmpeg_*.sh' -delete
-find build/openblas/ -mindepth 1 -not -name 'openblas_setup.sh' -delete
+#find build/ffmpeg/ -mindepth 1 -not -name 'ffmpeg_*.sh' -delete
+#find build/openblas/ -mindepth 1 -not -name 'openblas_setup.sh' -delete
 
 green "CLEAN WHEEL DIR"
 find create_wheel/cv2/ -type f -not -name '__init__.py' -delete
