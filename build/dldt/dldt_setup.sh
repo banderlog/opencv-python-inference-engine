6ec4245c70365ca988f58268d39752d5fe83f864
Borys Kabakov <b.a.kabakov@gmail.com>
2022-12-31 00:59:34 +0200

    dldt use flags

--- build/dldt/dldt_setup.sh
+++ build/dldt/dldt_setup.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 
 # https://github.com/openvinotoolkit/openvino/wiki/CMakeOptionsForCustomCompilation
-# https://github.com/openvinotoolkit/openvino/issues/4527
-#      -D ENABLE_OPENCV=OFF \
-# https://github.com/openvinotoolkit/openvino/issues/5100
-#      -D BUILD_SHARED_LIBS=OFF \
-#      -D BUILD_SHARED_LIBS=ON \
-# https://github.com/openvinotoolkit/openvino/issues/5209
-#      -D NGRAPH_TOOLS_ENABLE=OFF \
 cmake -D CMAKE_BUILD_TYPE=Release \
       -D THREADING=TBB \
-#      -D ENABLE_MKL_DNN=ON \
-#      -D GEMM=JIT \
       -D ENABLE_FASTER_BUILD=ON \
       -D ENABLE_LTO=ON \
-#      -D ENABLE_VPU=ON \
-#      -D ENABLE_MYRIAD=ON \
       -D ENABLE_SSE42=ON \
       -D ENABLE_AVX2=ON \
       -D ENABLE_AVX512F=OFF \
-#      -D BUILD_TESTS=OFF \
-#      -D ENABLE_ALTERNATIVE_TEMP=OFF \
       -D ENABLE_CLDNN=OFF \
-#      -D ENABLE_CLDNN_TESTS=OFF \
       -D ENABLE_DOCS=OFF \
       -D ENABLE_GAPI_TESTS=OFF \
-#      -D ENABLE_GNA=OFF \
       -D ENABLE_OPENCV=OFF \
       -D ENABLE_PROFILING_ITT=OFF \
       -D ENABLE_PYTHON=OFF \
       -D ENABLE_SAMPLES=OFF \
-#      -D ENABLE_SPEECH_DEMO=OFF \
       -D ENABLE_TESTS=OFF \
       -D GAPI_TEST_PERF=OFF \
-#      -D NGRAPH_ONNX_IMPORT_ENABLE=ON \
-#      -D NGRAPH_TEST_UTIL_ENABLE=OFF \
-#      -D NGRAPH_TOOLS_ENABLE=OFF \
-#      -D NGRAPH_UNIT_TEST_ENABLE=OFF \
+      -D ENABLE_INTEL_GNA=OFF \
+      -D ENABLE_INTEL_MYRIAD=ON \
+      -D ENABLE_INTEL_MYRIAD_COMMON=ON \
+      -D ENABLE_HETERO=OFF \
+      -D ENABLE_MULTI=OFF \
+      -D ENABLE_AUTO=OFF \
+      -D ENABLE_TEMPLATE=OFF \
+      -D ENABLE_AUTO_BATCH=OFF \
+      -D ENABLE_OV_ONNX_FRONTEND=ON \
+      -D ENABLE_OV_PDPD_FRONTEND=OFF \
+      -D ENABLE_OV_TF_FRONTEND=ON \
+      -D ENABLE_OV_IR_FRONTEND=OFF \
+      -D BUILD_TESTS=OFF \
       -D SELECTIVE_BUILD=OFF ../../dldt/
